8deb3e75e333e2458f006361454ff88d327ec54d
Luiz Fernando Dias Costa <luizfdias277@gmail.com>
2023-12-05 10:04:08 -0300

    adicionando exercício entregavel calculadora

--- src/script.js
+++ src/script.js
@@ -1,7 +1,39 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function calculadora(num1, num2, operador){
+
+  let resultado
+  let num1 = parseInt(num1)
+  let num2 = parseInt(num2)
+  
+  switch(operador){
+      case "+":
+          console.log(num1 + num2);
+          break;
+      case "-":
+          console.log(num1 - num2);
+          break;
+      case "*":
+          console.log(num1 * num2);
+          break;
+      case "/":
+          console.log(num1 / num2);
+          break;
+      case "e":
+          let contador = 1
+          resultado = num1 * num1
+          while (contador < num2 - 1) {
+              resultado = resultado * num1
+              contador++
+          }
+          break;
+  }
+  
+      if (resultado == undefined || resultado > 1000000)
+          resultado = "ERRO"
+      return resultado
+  }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
